d0e1dc9c78ca3669f06cb504bc08d8f7bbf6665c
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-14 21:52:30 +0530

    add

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector('#menu');
 const menu_list = document.querySelector('#menu-content');
-
+const first_div = document.querySelector('#first-div');
 menu.addEventListener('click', () => {
     menu.classList.toggle('open');
     menu_list.classList.toggle('open');
+    first_div.classList.toggle('open');
     console.log('click');
 });
