d0e33982e6d292ab9a9c2594b65604ba3f2475a8
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-17 22:46:18 +0530

    add

---
+++ js/index1.js
@@ -0,0 +1,49 @@
+const users = document.querySelector('#user');
+const company = document.querySelector('#company');
+const words = document.querySelector('#words');
+
+function updateUserCount() {
+    let user_count = 0; // Initialize user_count
+
+    const intervalId = setInterval(function() {
+        user_count += 1;
+        users.innerText = user_count;
+
+        if (user_count >= 360) {
+            clearInterval(intervalId);
+            users.innerText = '360+' // Stop the interval when user_count reaches 1000
+        }
+    }, 5);
+}
+
+function updateCompanyCount() {
+    let company_count = 0; // Initialize user_count
+
+    const intervalCompany = setInterval(function() {
+        company_count += 1;
+        company.innerText = company_count;
+
+        if (company_count >= 230) {
+            clearInterval(intervalCompany);
+            company.innerText = '230+'
+        }
+    }, 5);
+}
+
+function updateWordsCount() {
+    let words_count = 0;
+
+    const intervalWord = setInterval(function() {
+        words_count += 1;
+        words.innerText = '$'+words_count+'M';
+
+        if (words_count >= 230) {
+            clearInterval(intervalWord);
+            words.innerText = '$230M+';
+        }
+    },5);
+}
+
+updateUserCount();
+updateCompanyCount();
+updateWordsCount();
